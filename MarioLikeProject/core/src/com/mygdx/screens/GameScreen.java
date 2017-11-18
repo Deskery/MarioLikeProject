890b51bf55a9ddcfabb23832414f4663921db10b
Unknown <loick1994@hotmail.fr>
2017-11-18 22:31:42 +0100

    Ground

---
+++ MarioLikeProject/core/src/com/mygdx/screens/GameScreen.java
@@ -0,0 +1,62 @@
+package com.mygdx.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.mygdx.stages.GameStage;
+
+public class GameScreen implements Screen {
+
+	private GameStage stage;
+
+    public GameScreen() {
+        stage = new GameStage();
+    }
+
+    @Override
+    public void render(float delta) {
+        //Clear the screen
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        //Update the stage
+        stage.draw();
+        stage.act(delta);
+    }
+    
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
